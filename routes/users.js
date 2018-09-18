d74590a0ed466ac16f2dd948c187c6366a5dc065
mhmmtucan <mhmmtucan@gmail.com>
2018-09-18 14:05:27 +0300

    test for travis

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-   res.send('ok ok');
+    //res.setHeader('Content-Type', 'application/json');
+    // res.send(JSON.stringify({ status: 'ok'}));
+    res.jsonp({status: 'ok'});
 });
 
 
 router.get('/:userID', function(req, res, next) {
-  res.send(req.params.userID);
+    res.send(req.params.userID);
 });
 
 module.exports = router;
